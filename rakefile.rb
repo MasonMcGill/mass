790d8baea4254a9d47c8981e9420cc68226c73fb
Mason McGill <mason.b.mcgill@gmail.com>
2014-10-06 14:04:39 -0700

    Made "rakefile.rb" more portable.

--- rakefile.rb
+++ rakefile.rb
@@ -1,7 +1,8 @@
 COMPILE = "gcc -c --std=c99 -Iconf -O3 -o"
 LINK = "gcc -O3 -o"
-WITH_DEPS = "-lm $(pkg-config gstreamer-1.0 --cflags --libs)"
-WITH_CONF = "-DCONFIG="
+DEPS = "-lm " + `pkg-config gstreamer-1.0 --cflags --libs`
+CONF = "-DCONFIG="
+DELETE = "rm -f"
 
 configs = Dir["conf/*.h"].map {|p| File.basename p, ".*"}
 modules = Dir["src/*.c"].map {|p| File.basename p, ".*"}
@@ -10,19 +11,19 @@ configs.each do |c|
   obj_paths = modules.map {|m| "obj/#{c}_#{m}.o"}
   modules.each do |m|
     file "obj/#{c}_#{m}.o" => ["src/#{m}.c", "src/app.h", "conf/#{c}.h"] do
-      sh "#{COMPILE} obj/#{c}_#{m}.o src/#{m}.c #{WITH_CONF}#{c} #{WITH_DEPS}"
+      sh *"#{COMPILE}obj/#{c}_#{m}.o src/#{m}.c #{CONF}#{c} #{DEPS}".split
     end
   end
   file "bin/#{c}" => obj_paths do
-    sh "#{LINK} bin/#{c} #{obj_paths.join " "} #{WITH_DEPS}"
+    sh *"#{LINK} bin/#{c} #{obj_paths.join " "} #{DEPS}".split
   end
   task "build_#{c}" => "bin/#{c}"
   task "run_#{c}" => "build_#{c}" do
-    sh "bin/#{c}"
+    sh *"bin/#{c}".split
   end
 end
 
 task :default => :build
 task :build => :build_default
 task :run => :run_default
-task :clean do; sh "rm -rf obj/* bin/*"; end
+task :clean do; sh "#{DELETE} obj/* bin/*"; end
